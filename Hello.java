213e7f754d3e7d47dafb2b1a9ade02f38399d157
parub22 <parbatibudhathoki3@gmail.com>
2019-12-08 17:16:35 +0545

    message added

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("This is main method of Hello class");
+        System.out.println("This is main method of Hello class for Jenkins test");
         // TODO code application logic here
         for (int i = 0; i <= 10; i++) {
             System.out.println("hello world ..." + i);
